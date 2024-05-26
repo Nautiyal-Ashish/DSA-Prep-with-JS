8d8b98e53ba1b0c80bb3923ac7d6839f83b3f9b2
Ashun007 <157597734+Nautiyal-Ashish@users.noreply.github.com>
2024-05-26 09:42:23 +0530

    01 - Variables

---
+++ JS Fundamentals/01-variables.js
@@ -0,0 +1,20 @@
+//--------- Variables -----------------
+
+// 1. Var keyword
+var a = 10;
+var a = 20;
+// console.log(a);
+
+// 2. Let keyword
+let x = 2;
+// console.log(x);
+x = 5; //reassigned another value to x
+// console.log(x);
+
+// Const Keyword
+const id = 123;
+// console.log(id);
+id = 245; //❌
+// console.log(id); // will give error because const variables cannot be reassigned
+
+// -------------------
