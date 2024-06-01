309e43f134db0bc4d8f061873aa78f41df88f2d8
Ashun007 <157597734+Nautiyal-Ashish@users.noreply.github.com>
2024-06-01 18:29:28 +0530

    JS Fundamentals

---
+++ JS Fundamentals/Day-5/Functions.js
@@ -0,0 +1,11 @@
+// function name(params) {
+//     // code
+// }
+
+// Creating a function to sum values
+function addNumbers(val1, val2) {
+  let sum = val1 + val2;
+  console.log(sum);
+}
+
+addNumbers(2, 5);
