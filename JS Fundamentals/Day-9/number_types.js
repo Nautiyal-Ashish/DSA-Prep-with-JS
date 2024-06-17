3d3c2a344a0439a0e02d38221016b4ec441cefce
Ashun007 <157597734+Nautiyal-Ashish@users.noreply.github.com>
2024-06-17 09:50:00 +0530

    Day 9 - Numbers

---
+++ JS Fundamentals/Day-9/number_types.js
@@ -0,0 +1,25 @@
+// 1. The Only Numeric Type
+
+let onlyNumeric = 1;
+console.log(`OnlyNumeric : ${onlyNumeric}`);
+
+// 2. Scientific Notation 
+
+let num1 = 156e5;
+let num2 = 156e-5;
+console.log(`num1 : ${num1}`);
+console.log(`num2 : ${num2}`);
+
+// 3. Integer Precision
+
+let num3 = 9999999999999;
+let num4 = 99999999999999999;
+console.log(`num3 : ${num3}`);
+console.log(`num4 : ${num4}`);
+
+// 4. Floating Point Precision
+
+let num5 = 0.22 + 0.12;
+let num6 = (0.22 * 10 + 0.12 * 10) / 10;
+console.log(`num5 : ${num5}`);
+console.log(`num6 : ${num6}`);
